0e364e378e20f20a57eb2757538302260ec45617
kyao4 <kyao4@hawk.iit.edu>
2016-01-11 19:02:12 -0600

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # web_crawler
+
+This web crawler is designed specifically for scraping premium and annual revenue on insurance website.
+
+##Dependencies
+
++ [python +3.4.4](https://www.python.org/downloads/)
++ [beautifulsoup4 +4.4.1](http://www.crummy.com/software/BeautifulSoup/bs4/doc/)
++ [pdfminer3k +1.3.0](https://pypi.python.org/pypi/pdfminer3k/)
++ [selenium +2.48.0](http://www.seleniumhq.org/)
+
+##Quick Start
+
+1. First install [pip](https://pip.pypa.io/en/stable/installing/)
+then you can install all the dependencies by typing 
+
+pip install [Dependency name]
+
+on you terminal
+2. run main.py in default package with no parameter
+
+##Notes
+
++ list_of_url.csv is all the website that need to be scraped.
++ phantomjs.exe is a web browser used for executing JavaScript
++ test package is a crawler for scraping definition of words on wiki 
+you can run test.py to see the result
+
--- default/main.py
+++ default/main.py
@@ -12,7 +12,7 @@ import csv
 from selenium import webdriver
 import os
 import time
-
+import webbrowser
 
 class Website():
     def __init__(self, url):
@@ -20,24 +20,24 @@ class Website():
         self.__soup = self.__getSoup()
     
 
-    def __getSoup(self, relativeurl = ''):
+    def __getSoup(self, rel_url = ''):
         try:
-            conn = urlopen(self.__url + relativeurl)
+            conn = urlopen(self.__url + rel_url)
         except HTTPError as e: 
             # Server thinks this is a bot. so we use real broswer to access it!
             driver = webdriver.PhantomJS(executable_path = os.getcwd() + '\phantomjs.exe')
-            driver.get(self.__url + relativeurl)
-            time.sleep(3) # wait for JavaScript executing
+            driver.get(self.__url + rel_url)
+            time.sleep(1) # wait for JavaScript executing
             soup = BeautifulSoup(driver.page_source, "html.parser")
             driver.close()
             return soup
         except URLError as e:
-            print(e, self.__url)
+            print(e, self.__url + rel_url)
             return None
         try:
             soup = BeautifulSoup(conn.read(), "html.parser")
         except AttributeError as e:
-            print(e, self.__url)
+            print(e, self.__url + rel_url)
             return None
         return soup
     
@@ -48,13 +48,16 @@ class Website():
             return None
         return title
 
-
-# url = 'http://www.allstate.com/'
-# site = Website(url)
-# print(site.getTitle())
-with open('list_of_url.csv', 'rt') as urlFile:
-    csvReader = csv.reader(urlFile)
-    siteList = [ Website(url[0].strip()) for url in csvReader if url[0]]
-    for site in siteList:
-        print(site.getTitle())
+    def openSite(self, rel_url = ''):
+        webbrowser.open(self.__url + rel_url)
+url = 'http://www.allstate.com/'
+site = Website(url)
+print(site.getTitle())
+# with open('list_of_url.csv', 'rt') as urlFile:
+#     csvReader = csv.reader(urlFile)
+#     siteList = [ Website(url[0].strip()) for url in csvReader if url[0]]
+#     for site in siteList:
+#         print(site.getTitle())
+        
+         
     
