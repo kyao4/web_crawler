2c84b8d38f033fff23084c8cc1d8cfabe9dc55c4
kyao4 <kyao4@hawk.iit.edu>
2016-01-12 00:31:37 -0600

    get all internal links for first page

--- default/main.py
+++ default/main.py
@@ -13,13 +13,58 @@ from selenium import webdriver
 import os
 import time
 import webbrowser
+import re
 
 class Website():
     def __init__(self, url):
         self.__url = url # save root url
         self.__soup = self.__getSoup()
-    
+        
+        #variables for searching for premium (function:getPremium)
+        self.__currentSoup = self.__soup 
+        self.__targeturl = set()
+        self.__internalurl = set()
+        self.__externalurl = set()
+        
+        
+    def getPremium(self):
+        self.getTargeturl()
+        return 0
+        targetData = self.getTargetData()
+        result = 0
+        for v in targetData:
+            if v > result:
+                result = v
+        return result
 
+    def getTargeturl(self, rel_url = ''):
+        # first get all internal and external links 
+        #caution: add all the internals to internal set before filter
+        self.getInternalLinks('')
+        #filter all the links with keywords
+        
+        #add filtered links to target set
+        
+        #search all the internal set recursively
+        pass
+    
+    def getInternalLinks(self, includeurl):
+        for tag in self.__currentSoup.find_all('a', {'href': re.compile('^(/|.*' + self.splitRooturl()[0] + ')')}):
+            if tag.attrs['href'] not in self.__internalurl:
+                href = tag.attrs['href']
+                self.__internalurl.add(href)
+                print(href)
+    
+        
+    def getTargetData(self):
+        
+        pass
+    
+    def splitRooturl(self):
+        addressParts = self.__url.replace("http://", "").split("/")
+        return addressParts
+    
+      
     def __getSoup(self, rel_url = ''):
         try:
             conn = urlopen(self.__url + rel_url)
@@ -50,14 +95,21 @@ class Website():
 
     def openSite(self, rel_url = ''):
         webbrowser.open(self.__url + rel_url)
-url = 'http://www.allstate.com/'
-site = Website(url)
-print(site.getTitle())
-# with open('list_of_url.csv', 'rt') as urlFile:
-#     csvReader = csv.reader(urlFile)
-#     siteList = [ Website(url[0].strip()) for url in csvReader if url[0]]
-#     for site in siteList:
-#         print(site.getTitle())
+    
+    
+    
+        
+# url = 'http://www.allstate.com/'
+# site = Website(url)
+# print(site.getTitle())
+with open('list_of_url.csv', 'rt') as urlFile:
+    csvReader = csv.reader(urlFile)
+    #                                                  stop when shortest sequence is done
+    siteList = [ Website(url[0].strip())  for url,i in zip(csvReader, range(5)) if url[0].strip()]
+    for site in siteList:
+        print(site.getTitle())
+        print('premium', site.getPremium())
+        
         
          
     
